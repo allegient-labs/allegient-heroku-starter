361276d6c4d5a6bab6ec042583f5daeaa73614f0
Alex Bariyev <alexbariyev@users.noreply.github.com>
2017-11-20 11:52:06 -0500

    update

---
+++ config/express.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const glob = require('glob');
+
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compress = require('compression');
+const methodOverride = require('method-override');
+const exphbs  = require('express-handlebars');
+
+module.exports = (app, config) => {
+  const env = process.env.NODE_ENV || 'development';
+  app.locals.ENV = env;
+  app.locals.ENV_DEVELOPMENT = env == 'development';
+  if (app.get('env') === 'development') {
+    /* eslint-disable global-require */
+    require('dotenv').config();
+  } else if (app.get('env') === 'production') {
+    // Import the NewRelic Module.
+    //require('newrelic');
+    // Force https protocol for any connection
+    //app.use(forceSSL);
+    /* Ensure that the XFPHeader is trusted, otherwise can cause redirect loop */
+    // app.set('forceSSLOptions', {
+    //   trustXFPHeader: true,
+    //   sslRequireMessage: 'SSL Required.',
+    // });
+  } else {
+    console.log('Please set your NODE_ENV to either `development` or `production`');
+  }
+  app.engine('handlebars', exphbs({
+    layoutsDir: config.root + '/app/views/layouts/',
+    defaultLayout: 'main',
+    partialsDir: [config.root + '/app/views/partials/']
+  }));
+  app.set('views', config.root + '/app/views');
+  app.set('view engine', 'handlebars');
+
+  // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(cookieParser());
+  app.use(compress());
+  app.use(express.static(config.root + '/public'));
+  app.use(methodOverride());
+
+  var controllers = glob.sync(config.root + '/app/controllers/*.js');
+  controllers.forEach((controller) => {
+    require(controller)(app);
+  });
+
+  app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err,
+        title: 'error'
+      });
+    });
+  }
+
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {},
+      title: 'error'
+    });
+  });
+
+  return app;
+};
